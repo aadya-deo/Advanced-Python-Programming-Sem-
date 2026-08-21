9e65946c5916b5600a786f93c36f05e36aa0bf98
Aadya Deo <aadyadeo027@gmail.com>
2026-08-21 17:59:34 +0530

    Add files via upload

---
+++ fibonacciseries.py
@@ -0,0 +1,22 @@
+# recursive function -> memoization in dynamic programming
+def fiboRecur(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fiboRecur(n-1)+fiboRecur(n-2)
+    
+# iterative function -> tabulation in dynamic programming
+def fiboIter(n):
+    prev_n = 0
+    curr_n = 1
+    for i in range(1,n):
+        prevPrev_n = prev_n 
+        prev_n = curr_n
+        curr_n = prevPrev_n + prev_n
+    return curr_n
+
+n = int(input("Enter the number: "))
+print(f"Using recursion, value for fibo({n}) is {fiboRecur(n)}")
+print(f"Using iteration, value for fibo({n}) is {fiboIter(n)}")
